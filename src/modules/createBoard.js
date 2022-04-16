a27cea46d00f2fec82d84229bf78f19834732fa5
aspiringLilCoder <87404668+aspiringLilCoder@users.noreply.github.com>
2022-04-16 16:57:53 +0800

    Make a function that generates a 1000 squares

---
+++ src/modules/createBoard.js
@@ -0,0 +1,11 @@
+function createBoard(containerElement) {
+    let letters = 'ABCDEFGHIJ';
+    for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < 10; j++) {
+            let div = document.createElement('div');
+            div.classList.add('square');
+            div.classList.add(`${letters[i]}${j+1}`);
+            containerElement.appendChild(div);
+        }
+    }
+}
