db705972199a0c4303fdef5b026198b8d7a5f1ef
aspiringLilCoder <87404668+aspiringLilCoder@users.noreply.github.com>
2022-04-18 14:04:26 +0800

    Fix Cannot access 't' before initialization bug

--- src/modules/shipFactory.js
+++ src/modules/shipFactory.js
@@ -1,11 +1,11 @@
 
-export default function shipFactory(name, length, instanceName) {
+export default function shipFactory(name, length) {
     let placesHit = [];
     for (let i = 0; i < length; i++) {
         placesHit.push(false);
     }
 
-    const hit = (index) => {
+    const hit = (index, instanceName) => {
         let placesHit = instanceName.placesHit;
         placesHit[index] == undefined ? console.log('Hit is undefined') : placesHit[index] = true;
     } 
