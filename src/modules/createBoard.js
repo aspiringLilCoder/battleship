decc6af66c8897c696b29dc94b19c2c88fa829c4
aspiringLilCoder <87404668+aspiringLilCoder@users.noreply.github.com>
2022-04-18 22:30:53 +0800

    Make it so the listOfSquareClasses would include the class names of squares

--- src/modules/createBoard.js
+++ src/modules/createBoard.js
@@ -1,10 +1,13 @@
-export default function createBoard(containerElement) {
+export let listOfSquareClasses = [];
+
+export function createBoard(containerElement) {
     let letters = 'ABCDEFGHIJ';
     for (let i = 0; i < letters.length; i++) {
         for (let j = 0; j < 10; j++) {
             let div = document.createElement('div');
             div.classList.add('square');
             div.classList.add(`${letters[i]}${j+1}`);
+            listOfSquareClasses.push(`${letters[i]}${j+1}`);
             containerElement.appendChild(div);
         }
     }
