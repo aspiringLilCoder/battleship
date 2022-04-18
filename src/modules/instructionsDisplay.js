580354caa56bfa0778a49b12968f8cc42c083664
aspiringLilCoder <87404668+aspiringLilCoder@users.noreply.github.com>
2022-04-18 22:59:18 +0800

    Make it so that when it finishes displaying all the ships it will say 'press to start'

--- src/modules/instructionsDisplay.js
+++ src/modules/instructionsDisplay.js
@@ -1,8 +1,15 @@
 const instructionsText_p = document.getElementById('instructionsText');
+const instructionsDisplay_div = document.getElementById('instructionsDisplay');
 
-export let currentShip = "start";
+export let currentShip = 'Patrol Boat';
 
 export function instructionDisplay() {
+    if (currentShip == 'Patrol Boat') {
+        instructionsText_p.textContent = 'PRESS TO START';
+        instructionsDisplay_div.classList.add('start');
+        console.log(instructionsDisplay_div);
+        return;
+    }
     const shipOrder = ['start', 'Carrier', 'Battleship', 'Destroyer', 'Submarine', 'Patrol Boat'];
     instructionsText_p.textContent = `Place your ${shipOrder[shipOrder.indexOf(currentShip) + 1]}...`;
     instructionsText_p.classList.add('changingDisplay');
